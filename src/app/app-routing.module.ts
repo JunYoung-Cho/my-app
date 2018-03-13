921a768c553c33564ab53b57d96781e236a2f2f5
JunYoung-Cho <cjy8745@ilts.co.kr>
2018-03-13 22:43:00 +0900

    reset

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
-import {HeroesComponent} from './heroes/heroes.component';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-    {path: 'heroes', component: HeroesComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {HttpClientModule }    from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 
 
 import {AppComponent} from './app.component';
-import {HeroesComponent} from './heroes/heroes.component';
-import {HeroesDetailsComponent} from './heroes-details/heroes-details.component';
 
-import {HeroService} from './service/hero.service';
-import {MassageService} from './service/massage.service';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryDataService} from './in-memory-data.service';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        HeroesComponent,
-        HeroesDetailsComponent
+        AppComponent
     ],
     imports: [
         BrowserModule,
@@ -28,9 +22,7 @@ import {InMemoryDataService} from './in-memory-data.service';
         HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false})
     ],
     providers: [
-        HeroService,
-        MassageService,
-        InMemoryDataService,
+        InMemoryDataService
     ],
     bootstrap: [AppComponent]
 })
